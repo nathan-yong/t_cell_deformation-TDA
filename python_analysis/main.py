a1e0e8f873177ed92798a12219ee8260afa10b64
Nathan <nathanyong04@gmail.com>
2026-02-13 13:15:11 -0500

    works

--- python_analysis/main.py
+++ python_analysis/main.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 import yaml
+import re
 
 # Local imports
 import utils.progress_bar as progress_bar
-from utils.data_reader import read_particle_data
+import utils.data_reader as data_reader
+import utils.data_printer as data_printer
 from tda.alpha_complexes import alpha_complexes_with_particle_coords
 
 
@@ -58,6 +60,9 @@ def main():
       os.makedirs(experiment_analyses_dir)
   
   with os.scandir(data_dir) as experiment:
+    
+    is_first_experiment_sample_print = True
+    # LOOP: experiment sample
     for experiment_sample in experiment:
       if experiment_sample.name == 'analyses':
         continue
@@ -66,6 +71,7 @@ def main():
       realization_iterator = 0
       is_yaml_retrieved = False
       experiment_sample_analyses_dir = os.path.join(data_dir, experiment_sample.name, 'analyses')
+      
       if not os.path.exists(experiment_sample_analyses_dir):
           os.makedirs(experiment_sample_analyses_dir)
           
@@ -73,6 +79,9 @@ def main():
       with os.scandir(experiment_sample) as realizations:
         if experiment_sample.name == 'analyses':
           continue
+        
+        is_first_realization_print = True
+        # LOOP: realizations
         for realization_sample in realizations:
           if realization_sample.name == 'analyses':
             continue 
@@ -81,20 +90,38 @@ def main():
           if not os.path.exists(realization_analyses_dir):
               os.makedirs(realization_analyses_dir)
 
-          realization_data = read_particle_data(os.path.join(data_dir, experiment_sample.name, realization_sample.name, 'Results.txt'), 
-                                                NUM_PARTICLES, 
-                                                NUM_TIMESTAMPS, 
-                                                include_apc=INCLUDE_APC, 
-                                                skip_init_frame=SKIP_INIT_FRAME)
+          realization_data = data_reader.read_particle_data(os.path.join(data_dir, experiment_sample.name, realization_sample.name, 'Results.txt'), 
+                                                            NUM_PARTICLES, 
+                                                            NUM_TIMESTAMPS, 
+                                                            include_apc=INCLUDE_APC, 
+                                                            skip_init_frame=SKIP_INIT_FRAME)
           
           # Deploy all methods selected in the analysis_config
           if analysis_config['alpha_complexes_with_particle_coords']:
             alpha_complexes_with_particle_coords(realization_data, realization_analyses_dir, analysis_config['timestamp_difference'], SKIP_INIT_FRAME)
             # print to experiment_sample_analyses_dir. Copy the files created by function.
+            data_printer.print_experiment_sample(os.path.join(realization_analyses_dir, "alpha_complexes_with_particle_coords_dim0.txt"), 
+                                                experiment_sample_analyses_dir, 
+                                                "freq" + str(experiment_sample.name) + "_alpha_complexes_with_particle_coords_dim0.txt",
+                                                realization_sample.name, is_first_realization_print)
+            data_printer.print_experiment_sample(os.path.join(realization_analyses_dir, "alpha_complexes_with_particle_coords_dim1.txt"), 
+                                                experiment_sample_analyses_dir, 
+                                                "freq" + str(experiment_sample.name) + "_alpha_complexes_with_particle_coords_dim1.txt",
+                                                realization_sample.name, is_first_realization_print)
+            is_first_realization_print = False
           realization_iterator += 1
 
       # print to experiment_analyses_dir. Copy realization file.
+      data_printer.print_experiment(os.path.join(experiment_sample_analyses_dir, f"freq{experiment_sample.name}_alpha_complexes_with_particle_coords_dim0.txt"), 
+                                    experiment_analyses_dir, 
+                                    f"{analysis_config['experiment_name']}_alpha_complexes_with_particle_coords_dim0.txt",
+                                    re.findall(r'\d+', experiment_sample.name)[0], is_first_experiment_sample_print)
+      data_printer.print_experiment(os.path.join(experiment_sample_analyses_dir, f"freq{experiment_sample.name}_alpha_complexes_with_particle_coords_dim1.txt"), 
+                                    experiment_analyses_dir, 
+                                    f"{analysis_config['experiment_name']}_alpha_complexes_with_particle_coords_dim1.txt",
+                                    re.findall(r'\d+', experiment_sample.name)[0], is_first_experiment_sample_print)
 
+      is_first_experiment_sample_print = False
       frequency_iterator+=1
   
   progress_bar.stop_progress_bar()
--- python_analysis/utils/data_printer.py
+++ python_analysis/utils/data_printer.py
@@ -14,8 +14,27 @@ def print_realizaton(filename, measure_data, realization_analyses_dir, timestamp
   with open(os.path.join(realization_analyses_dir, filename), 'w') as f:
     for i in range(len(measure_data)):
       if is_first_frame_skipped:
-        f.write(f'Timestamp: {(i+1) * timestamp_difference}\n')
+        f.write(f'Timestamp {(i+1) * timestamp_difference}\n')
       else:
-        f.write(f'Timestamp: {i * timestamp_difference}\n')
+        f.write(f'Timestamp {i * timestamp_difference}\n')
       for measure in measure_data[i]:
         f.write(f'{measure}\n')
+
+# Copy contents of realization file to experiment sample analyses directory file append to existing file
+def print_experiment_sample(realization_file, experiment_sample_analyses_dir, experiment_name, realization_sample_name, is_first_print):
+  write_or_append = 'a'
+  if is_first_print:
+    write_or_append = 'w'
+  with open(os.path.join(experiment_sample_analyses_dir, f"{experiment_name}"), write_or_append) as f:
+    f.write(f"Realization {realization_sample_name}\n")
+    with open(realization_file, 'r') as rf:
+      f.write(rf.read())
+      
+def print_experiment(experiment_sample_file, experiment_analyses_dir, experiment_name, experiment_sample_name, is_first_print):
+  write_or_append = 'a'
+  if is_first_print:
+    write_or_append = 'w'
+  with open(os.path.join(experiment_analyses_dir, f"{experiment_name}"), write_or_append) as f:
+    f.write(f"Experiment_Sample {experiment_sample_name}\n")
+    with open(experiment_sample_file, 'r') as ef:
+      f.write(ef.read())
